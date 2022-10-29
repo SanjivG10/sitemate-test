e0cf2778a80b32ab38d4469c132389b26504ced2
sanjiv <sanjivgautamofficial@gmail.com>
2022-10-29 22:37:57 +0545

    makes modal message dynamic

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const modals = document.querySelectorAll('[data-modal]');
+let buttonClickedByUser = null;
+const DEFAULT_MODAL_MESSAGE = "Are you sure you want to continue?";
 
-modals.forEach(function (trigger) {
-    trigger.addEventListener('click', function (e) {
-        e.preventDefault();
-        const modal = document.getElementById(trigger.dataset.modal);
-        modal.classList.add('open');
-        const msgContainer = document.getElementsByClassName("msg-container")[0];
+const initModal = (modalMessage = DEFAULT_MODAL_MESSAGE) => {
 
-        const containerButtons = document.getElementsByClassName("container-button");
-        for (const button of containerButtons) {
-            button.addEventListener("click", () => {
-                modal.classList.remove('open');
-                msgContainer.innerHTML = `You just clicked '${button.textContent || button.innerText}'`
-                msgContainer.style.display = "block";
-            })
-        }
+    modals.forEach(function (trigger) {
+        trigger.addEventListener('click', function (e) {
+            e.preventDefault();
+            const modal = document.getElementById(trigger.dataset.modal);
+            modal.classList.add('open');
+            const msgContainer = document.getElementsByClassName("msg-container")[0];
+            const modalContainer = document.querySelector(".modal-container h4");
+            modalContainer.innerHTML = modalMessage;
+
+            const containerButtons = document.getElementsByClassName("container-button");
+            for (const button of containerButtons) {
+                button.addEventListener("click", () => {
+                    modal.classList.remove('open');
+                    const buttonName = button.textContent || button.innerText
+                    msgContainer.innerHTML = `You just clicked "${buttonName}"`
+                    buttonClickedByUser = buttonName;
+                    msgContainer.style.display = "block";
+                })
+            }
+        });
     });
-});
+}
+
+initModal();
+
 
