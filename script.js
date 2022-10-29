19949faff150126ef57501bc6476839236d3544b
sanjiv <sanjivgautamofficial@gmail.com>
2022-10-29 22:56:33 +0545

    makes buttons dynamic; adds little bit of styling

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
-const modals = document.querySelectorAll('[data-modal]');
-let buttonClickedByUser = null;
+const trigger = document.querySelector('[data-modal]');
+const msgContainer = document.getElementsByClassName("msg-container")[0];
 const DEFAULT_MODAL_MESSAGE = "Are you sure you want to continue?";
 
-const initModal = (modalMessage = DEFAULT_MODAL_MESSAGE) => {
 
-    modals.forEach(function (trigger) {
-        trigger.addEventListener('click', function (e) {
-            e.preventDefault();
-            const modal = document.getElementById(trigger.dataset.modal);
-            modal.classList.add('open');
-            const msgContainer = document.getElementsByClassName("msg-container")[0];
-            const modalContainer = document.querySelector(".modal-container h4");
-            modalContainer.innerHTML = modalMessage;
+const addButton = (buttonName, onClick) => {
+    const button = document.createElement("button");
+    button.innerHTML = buttonName;
+    button.onclick = () => {
+        const modal = document.getElementById(trigger.dataset.modal);
+        modal.classList.remove('open');
+        onClick();
+    };
+    return button;
+}
 
-            const containerButtons = document.getElementsByClassName("container-button");
-            for (const button of containerButtons) {
-                button.addEventListener("click", () => {
-                    modal.classList.remove('open');
-                    const buttonName = button.textContent || button.innerText
-                    msgContainer.innerHTML = `You just clicked "${buttonName}"`
-                    buttonClickedByUser = buttonName;
-                    msgContainer.style.display = "block";
-                })
-            }
-        });
+const initModal = (buttons, modalMessage = DEFAULT_MODAL_MESSAGE) => {
+
+    trigger.addEventListener('click', function (e) {
+        e.preventDefault();
+        const modal = document.getElementById(trigger.dataset.modal);
+        modal.classList.add('open');
+        const modalContainer = document.querySelector(".modal-container h4");
+        modalContainer.innerHTML = modalMessage;
+
+        const allButtonsContainer = document.getElementsByClassName("flex")[0];
+        for (const button of buttons) {
+            allButtonsContainer.appendChild(button);
+        }
+        // allButtonsContainer
     });
 }
 
-initModal();
+const yesButton = addButton("Yes", () => {
+    msgContainer.innerHTML = `You just clicked "Yes"`
+    msgContainer.style.display = "block";
+})
+
+
+const cancelButton = addButton("Cancel", () => {
+    msgContainer.innerHTML = `You just clicked "Cancel"`
+    msgContainer.style.display = "block";
+})
+
+const allButtons = [yesButton, cancelButton]
+
+initModal(allButtons);
 
 
